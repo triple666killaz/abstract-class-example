78b94e14e13143b5cbcf03c2c309092517f61354
triple666killaz <blackrascal14@gmail.com>
2022-06-05 20:05:08 +0300

    First commit

---
+++ AbstractClassExample/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClassExample
+{
+    abstract class Animal
+    {
+
+        private string name = "Just animal";
+
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+        public abstract void Move();
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+
+    }
+}
